09027f80fd9d6db71e8e9ab61c883fe277465fac
Ananthu27 <kma270999@gmail.com>
2024-03-29 15:32:15 +0530

    added threding to lemmatization

--- main.py
+++ main.py
@@ -5,19 +5,29 @@ config = {
     'text_cols' : ['Text']
 }
 
-# READING DATA
-data_path = './data/'
-df = pd.read_csv(data_path+config['filename'])
-df = df[:5]
+if __name__ =='__main__' :
+    
+    # READING DATA
+    data_path = './data/'
+    df = pd.read_csv(data_path+config['filename'])
+    df = df[:100]
 
-# 1 LOWERCASE
-from preprocess import to_lower
-for col in config['text_cols'] : 
-    df[col] = df[col].apply(to_lower)
+    # 1 LOWERCASE
+    from preprocess import to_lower
+    for col in config['text_cols'] : 
+        df[col] = df[col].apply(to_lower)
 
-# 3 LEMMATIZATION 
-from preprocess import lemmatize
-for col in config['text_cols']:
-    df[col] = df[col].apply(lemmatize)
+    # 3 LEMMATIZATION 
+    from preprocess import lemmatize
 
-print (df.head())
+    def lemmatize_row(row):
+        for col in config['text_cols'] :
+            row[col] = lemmatize(row[col])
+        return row
+
+    # applying lemmatization in a thread pool
+    import concurrent.futures
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = list(executor.map(lemmatize_row, df.to_dict(orient='records')))
+    df = pd.DataFrame(results)
+    print (df.head())
